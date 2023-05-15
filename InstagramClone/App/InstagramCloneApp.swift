dcba612ac08fa1dd3fa05d226633f46099ee0e9d
Lucas Newlands <lucasnewlands@MacBook-Pro-de-Lucas.local>
2023-05-15 20:51:49 -0300

    project setup

---
+++ InstagramClone/App/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Lucas Newlands on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
